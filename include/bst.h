cc44893ad5f46718a00e70d1c4e2d8552a34d894
SupeRWiNdz <125024771+SupeRWiNdz@users.noreply.github.com>
2023-05-30 20:18:50 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,62 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <iostream>
+
+template <typename T>
+class BST {
+ private:
+    struct Node {
+        T val;
+        unsigned int count;
+        Node* l;
+        Node* r;
+    };
+    Node* root = nullptr;
+    int searchNodeTree(Node* node, const T& value) {
+        if (node == nullptr) {
+            return 0;
+        } else if (node->val > value) {
+            return searchNodeTree(node->l, value);
+        } else if (node->val < value) {
+            return searchNodeTree(node->r, value);
+        } else {
+            return node->count;
+        }
+        return 0;
+    }
+    int getDepthTree(Node* node) {
+        if (node == nullptr)
+            return 0;
+        int rDepth = getDepthTree(node->r),
+            lDepth = getDepthTree(node->l);
+        return (lDepth > rDepth) ? lDepth + 1 : rDepth + 1;
+    }
+    Node* newNodeTree(Node* root, const T& value) {
+        if (root == nullptr) {
+            root = new Node;
+            root->val = value;
+            root->count = 1;
+            root->l = root->r = nullptr;
+        } else if (root->val < value) {
+            root->r = newNodeTree(root->r, value);
+        } else if (root->val > value) {
+            root->l = newNodeTree(root->l, value);
+        } else {
+            root->count+=1;
+        }
+        return root;
+    }
+ public:
+    int search(const T& value) {
+        return searchNodeTree(node, value);
+    }
+    int depth() {
+        return getDepthTree(node) - 1;
+    }
+    void newNode(const T& value) {
+        node = newNodeTree(node, value);
+    }
+};
 
 #endif  // INCLUDE_BST_H_
